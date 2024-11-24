1ce5b6078f66271787b1b82c86084142b7a1687d
Anton Jouline <juce66@gmail.com>
2024-11-24 11:38:16 -0800

    adding fs.make_dirs function

--- fs.lua
+++ fs.lua
@@ -6,6 +6,7 @@ local m = {}
 
 if ffi ~= nil then
     ffi.cdef([[
+typedef int BOOL;
 typedef uint16_t WCHAR;
 typedef uint16_t WORD;
 typedef uint32_t UINT;
@@ -38,6 +39,7 @@ BOOL FindNextFileW(HANDLE hFindFile, LPWIN32_FIND_DATAW lpFindFileData);
 BOOL FindClose(HANDLE hFindFile);
 
 typedef uint16_t* LPWSTR;
+typedef uint16_t LPCWSTR;
 typedef char* LPSTR;
 typedef char* LPCCH;
 typedef uint8_t* LPBOOL;
@@ -45,18 +47,69 @@ typedef uint8_t* LPBOOL;
 int MultiByteToWideChar(UINT CodePage, DWORD dwFlags, char *lpMultiByteStr, int cbMultiByte, LPWSTR lpWideCharStr, int cchWideChar);
 int WideCharToMultiByte(UINT CodePage, DWORD dwFlags, LPWSTR lpWideCharStr, int cchWideChar, LPSTR lpMultiByteStr, int cbMultiByte,
         LPCCH lpDefaultChar, LPBOOL lpUsedDefaultChar);
+BOOL CreateDirectoryW(LPCWSTR *lpPathName, void *lpSecurityAttributes);
+int GetLastError();
     ]])
 end
 
-function m.find_files(pattern)
-    -- sanity check: need FFI
-    if ffi == nil then
-        log('WARNING: find_files requires Luajit FFI. Set luajit.ext.enabled = 1 in your sider.ini')
-        return function()
-            return nil, nil
-        end
+local CP_UTF8 = 65001
+local ERROR_UNKNOWN = -1
+local ERROR_PATH_NOT_FOUND = 3
+local ERROR_ALREADY_EXISTS = 183
+local NULL = ffi.new('void*', nil)
+
+local dir_errors = {
+    [ERROR_UNKNOWN] = "Unexpected error",
+    [ERROR_ALREADY_EXISTS] = "Directory already exists",
+    [ERROR_PATH_NOT_FOUND] = "Path not found",
+}
+
+local function make_dir(pathname)
+    local char_str = ffi.cast('char*', pathname)
+    local wide_char_str = ffi.new('uint16_t[?]', #pathname+1)
+    if ffi.C.MultiByteToWideChar(CP_UTF8, 0, char_str, #pathname, wide_char_str, #pathname+1) == 0 then
+        return 'MultiByteToWideChar returned error for pathname'
     end
 
+    if ffi.C.CreateDirectoryW(wide_char_str, NULL) == 0 then
+        return ffi.C.GetLastError()
+    end
+end
+
+local function make_dirs(pathname)
+    -- start with outermost dir: try to create that first
+    local err = make_dir(pathname)
+    if err == nil then
+        return
+    end
+
+    -- some other error: just return that
+    if err ~= ERROR_PATH_NOT_FOUND then
+        return err
+    end
+
+    -- try to create intermediate directories
+    local parent_dir, _ = string.match(pathname, "(.*)[\\/](.+)")
+    if not parent_dir then
+        return ERROR_UNKNOWN
+    end
+    err = make_dirs(parent_dir)
+    if err ~= nil and err ~= ERROR_ALREADY_EXISTS then
+        return err
+    end
+
+    -- now try one more time
+    return make_dir(pathname)
+end
+
+function m.make_dirs(pathname)
+    local err = make_dirs(pathname)
+    if err ~= nil then
+        return dir_errors[err] or string.format("Error code: %s", err)
+    end
+end
+
+function m.find_files(pattern)
     local CP_UTF8 = 65001
     local FILE_ATTRIBUTE_DIRECTORY = 0x10
 
